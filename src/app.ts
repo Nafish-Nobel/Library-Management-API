3f4a439a0ba504b3d6c5c2a070819cf613193181
Nur Nafish Ahmed Nobel <86622356+Nafish-Nobel@users.noreply.github.com>
2025-06-21 19:29:47 +0600

    update

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,15 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { router } from './app/routes';
-import { globalErrorHandler } from './app/middlewares/errorHandler';
-import serverless from 'serverless-http';
-import './config/db';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', router);
-app.use(globalErrorHandler);
+app.use('/', router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Library Management API is Running!');
 });
 
-
-export const handler = serverless(app);
+export default app;
