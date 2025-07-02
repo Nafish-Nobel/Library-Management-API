9aee674da4620ebbaa0369b00c1de5a702e2031e
Nur Nafish Ahmed Nobel <86622356+Nafish-Nobel@users.noreply.github.com>
2025-07-03 01:13:57 +0600

    new

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,16 @@ import { router } from './app/routes';
 
 const app = express();
 
-// app.use(cors(
-//   {
-//     origin: ["https://library-management-api-vert.vercel.app/", "http://localhost:5173"],
-//     methods: ["POST", "GET", "DELETE", "PUT"],
-//     credentials: true
-//   }
-// ));
+app.use(cors(
+  {
+    origin: ["https://library-management-api-vert.vercel.app", "http://localhost:5173"],
+    methods: ["POST", "GET", "DELETE", "PUT"],
+    credentials: true
+  }
+));
 // app.use(express.json({ limit: "25mb" }));
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 app.use('/api', router);
